627292669202540f781fdaa2ff2545155e60b0ee
John O'Sullivan <john@eximchain.com>
2019-08-26 01:59:55 -0400

    Updates

---
+++ precache-manifest.ffe2740f917fa83c988b967600fce7f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dda237b16798f6502c3ac5a984250d87",
+    "url": "/septdebates/index.html"
+  },
+  {
+    "revision": "0ba1b752126cee2b2998",
+    "url": "/septdebates/static/css/main.96b98fba.chunk.css"
+  },
+  {
+    "revision": "ed9dc48894818fbd62cc",
+    "url": "/septdebates/static/js/2.5326a3f0.chunk.js"
+  },
+  {
+    "revision": "0ba1b752126cee2b2998",
+    "url": "/septdebates/static/js/main.2e951bc0.chunk.js"
+  },
+  {
+    "revision": "fffe4dd508358302a7ae",
+    "url": "/septdebates/static/js/runtime~main.56d6d7da.js"
+  }
+]);
